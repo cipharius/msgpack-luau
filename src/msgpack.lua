0edc371c91b17f48d0a71a0001fef41388dbf542
cipharius <cipharius@pm.me>
2021-10-26 19:53:55 +0300

    Cleans up code and improves variable names

--- src/msgpack.lua
+++ src/msgpack.lua
@@ -7,64 +7,69 @@ local extract = bit32.extract
 local ldexp = math.ldexp
 
 local parse
-function parse(str, ofs)
-  local b = str:byte(ofs + 1, ofs + 1)
+function parse(message, offset)
+  local byte = message:byte(offset + 1, offset + 1)
 
-  if b == 0xC0 then     -- nil
-    return nil, ofs + 1
+  if byte == 0xC0 then     -- nil
+    return nil, offset + 1
 
-  elseif b == 0xC2 then -- false
-    return false, ofs + 1
+  elseif byte == 0xC2 then -- false
+    return false, offset + 1
 
-  elseif b == 0xC3 then -- true
-    return true, ofs + 1
+  elseif byte == 0xC3 then -- true
+    return true, offset + 1
 
-  elseif b == 0xC4 then -- bin 8
-    local len = str:byte(ofs + 2)
-    return {str:byte(ofs + 3, ofs + 2 + len)}, ofs + 2 + len
+  elseif byte == 0xC4 then -- bin 8
+    local length = message:byte(offset + 2)
+    return {message:byte(offset + 3, offset + 2 + length)},
+           offset + 2 + length
 
-  elseif b == 0xC5 then -- bin 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    local len = bor(
+  elseif byte == 0xC5 then -- bin 16
+    local i0,i1 = message:byte(offset + 2, offset + 3)
+    local length = bor(
       lshift(i0, 8),
       i1
     )
 
-    return {str:byte(ofs + 4, ofs + 3 + len)}, ofs + 3 + len
+    return {message:byte(offset + 4, offset + 3 + length)},
+           offset + 3 + length
 
-  elseif b == 0xC6 then -- bin 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    local len = bor(
+  elseif byte == 0xC6 then -- bin 32
+    local i0,i1,i2,i3 = message:byte(offset + 2, offset + 5)
+    local length = bor(
       lshift(i0, 24),
       lshift(i1, 16),
       lshift(i2, 8),
       i3
     )
 
-    return {str:byte(ofs + 6, ofs + 5 + len)}, ofs + 5 + len
+    return {message:byte(offset + 6, offset + 5 + length)},
+           offset + 5 + length
 
-  elseif b == 0xC7 then -- ext 8
-    local len = str:byte(ofs + 2)
+  elseif byte == 0xC7 then -- ext 8
+    local length = message:byte(offset + 2)
     return {
-      type = str:byte(ofs + 3),
-      data = str:sub(ofs + 4, ofs + 3 + len)
-    }, ofs + 3 + len
+            type = message:byte(offset + 3),
+            data = message:sub(offset + 4, offset + 3 + length)
+          },
+          offset + 3 + length
 
-  elseif b == 0xC8 then -- ext 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    local len = bor(
+  elseif byte == 0xC8 then -- ext 16
+    local i0,i1 = message:byte(offset + 2, offset + 3)
+    local length = bor(
       lshift(i0, 8),
       i1
     )
 
     return {
-      type = str:byte(ofs + 4),
-      data = str:sub(ofs + 5, ofs + 4 + len)
-    }, ofs + 4 + len
+             type = message:byte(offset + 4),
+             data = message:sub(offset + 5, offset + 4 + length)
+           },
+          offset + 4 + length
 
-  elseif b == 0xC9 then -- ext 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    local len = bor(
+  elseif byte == 0xC9 then -- ext 32
+    local i0,i1,i2,i3 = message:byte(offset + 2, offset + 5)
+    local length = bor(
       lshift(i0, 24),
       lshift(i1, 16),
       lshift(i2, 8),
@@ -72,12 +77,13 @@ function parse(str, ofs)
     )
 
     return {
-      type = str:byte(ofs + 6),
-      data = str:sub(ofs + 7, ofs + 6 + len)
-    }, ofs + 6 + len
+             type = message:byte(offset + 6),
+             data = message:sub(offset + 7, offset + 6 + length)
+           },
+           offset + 6 + length
 
-  elseif b == 0xCA then -- float 32
-    local f0, f1, f2, f3 = str:byte(ofs + 2, ofs + 5)
+  elseif byte == 0xCA then -- float 32
+    local f0,f1,f2,f3 = message:byte(offset + 2, offset + 5)
     local f = bor(
       lshift(f0, 24),
       lshift(f1, 16),
@@ -100,10 +106,11 @@ function parse(str, ofs)
       sum += ldexp(extract(f, 23 - i), -i)
     end
 
-    return ldexp(sign * sum, exponent - 127 ), ofs + 5
+    return ldexp(sign * sum, exponent - 127 ),
+           offset + 5
 
-  elseif b == 0xCB then -- float 64
-    local f0, f1, f2, f3, f4, f5, f6, f7 = str:byte(ofs + 2, ofs + 9)
+  elseif byte == 0xCB then -- float 64
+    local f0,f1,f2,f3,f4,f5,f6,f7 = message:byte(offset + 2, offset + 9)
     local fA = bor(
       lshift(f0, 24),
       lshift(f1, 16),
@@ -136,53 +143,49 @@ function parse(str, ofs)
       end
     end
 
-    return ldexp(sign * sum, exponent - 1023 ), ofs + 9
+    return ldexp(sign * sum, exponent - 1023 ),
+           offset + 9
 
-  elseif b == 0xCC then -- uint 8
-    return str:byte(ofs + 2), ofs + 2
+  elseif byte == 0xCC then -- uint 8
+    return message:byte(offset + 2),
+           offset + 2
 
-  elseif b == 0xCD then -- uint 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    return bor(
-      lshift(i0, 8),
-      i1
-    ), ofs + 3
+  elseif byte == 0xCD then -- uint 16
+    local i0,i1 = message:byte(offset + 2, offset + 3)
+    return bor(lshift(i0, 8), i1),
+           offset + 3
 
-  elseif b == 0xCE then -- uint 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    return bor(
-      lshift(i0, 24),
-      lshift(i1, 16),
-      lshift(i2, 8),
-      i3
-    ), ofs + 5
+  elseif byte == 0xCE then -- uint 32
+    local i0,i1,i2,i3 = message:byte(offset + 2, offset + 5)
+    return bor(lshift(i0, 24), lshift(i1, 16), lshift(i2, 8), i3),
+           offset + 5
 
-  elseif b == 0xCF then -- uint 64
+  elseif byte == 0xCF then -- uint 64
     error("Luau does not support uint 64")
 
-  elseif b == 0xD0 then -- int 8
-    local i = str:byte(ofs + 2)
+  elseif byte == 0xD0 then -- int 8
+    local i = message:byte(offset + 2)
     if i <= 127 then
-      return i, ofs + 2
+      return i, offset + 2
     else
-      return i - 256, ofs + 2
+      return i - 0x100, offset + 2
     end
 
-  elseif b == 0xD1 then -- int 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
+  elseif byte == 0xD1 then -- int 16
+    local i0, i1 = message:byte(offset + 2, offset + 3)
     local i = bor(
       lshift(i0, 8),
       i1
     )
 
     if i0 <= 127 then
-      return i, ofs + 3
+      return i, offset + 3
     else
-      return i - 65536, ofs + 3
+      return i - 0x10000, offset + 3
     end
 
-  elseif b == 0xD2 then -- int 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
+  elseif byte == 0xD2 then -- int 32
+    local i0, i1, i2, i3 = message:byte(offset + 2, offset + 5)
     local i = bor(
       lshift(i0, 24),
       lshift(i1, 16),
@@ -191,179 +194,188 @@ function parse(str, ofs)
     )
 
     if i0 <= 127 then
-      return i, ofs + 5
+      return i, offset + 5
     else
-      return i - 2147483648, ofs + 5
+      return i - 0x80000000, offset + 5
     end
 
-  elseif b == 0xD3 then -- int 64
-    error("Roblox does not support int 64")
+  elseif byte == 0xD3 then -- int 64
+    error("Luau does not support int 64")
 
-  elseif b == 0xD4 then -- fixext 1
+  elseif byte == 0xD4 then -- fixext 1
     return {
-      type = str:byte(ofs + 2),
-      data = str:sub(ofs + 3, ofs + 3)
-    }, ofs + 3
+             type = message:byte(offset + 2),
+             data = message:sub(offset + 3, offset + 3)
+           },
+           offset + 3
 
-  elseif b == 0xD5 then -- fixext 2
+  elseif byte == 0xD5 then -- fixext 2
     return {
-      type = str:byte(ofs + 2),
-      data = str:sub(ofs + 3, ofs + 4)
-    }, ofs + 4
+             type = message:byte(offset + 2),
+             data = message:sub(offset + 3, offset + 4)
+           },
+           offset + 4
 
-  elseif b == 0xD6 then -- fixext 4
+  elseif byte == 0xD6 then -- fixext 4
     return {
-      type = str:byte(ofs + 2),
-      data = str:sub(ofs + 3, ofs + 6)
-    }, ofs + 6
+             type = message:byte(offset + 2),
+             data = message:sub(offset + 3, offset + 6)
+           },
+           offset + 6
 
-  elseif b == 0xD7 then -- fixext 8
+  elseif byte == 0xD7 then -- fixext 8
     return {
-      type = str:byte(ofs + 2),
-      data = str:sub(ofs + 3, ofs + 10)
-    }, ofs + 10
+             type = message:byte(offset + 2),
+             data = message:sub(offset + 3, offset + 10)
+           },
+           offset + 10
 
-  elseif b == 0xD8 then -- fixext 16
+  elseif byte == 0xD8 then -- fixext 16
     return {
-      type = str:byte(ofs + 2),
-      data = str:sub(ofs + 3, ofs + 18)
-    }, ofs + 18
+             type = message:byte(offset + 2),
+             data = message:sub(offset + 3, offset + 18)
+           },
+           offset + 18
 
-  elseif b == 0xD9 then -- str 8
-    local len = str:byte(ofs + 2)
-    return str:sub(ofs + 3, ofs + 2 + len), ofs + 2 + len
+  elseif byte == 0xD9 then -- str 8
+    local length = message:byte(offset + 2)
+    return message:sub(offset + 3, offset + 2 + length),
+           offset + 2 + length
 
-  elseif b == 0xDA then -- str 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    local len = bor(
+  elseif byte == 0xDA then -- str 16
+    local i0, i1 = message:byte(offset + 2, offset + 3)
+    local length = bor(
       lshift(i0, 8),
       i1
     )
 
-    return str:sub(ofs + 4, ofs + 3 + len), ofs + 3 + len
+    return message:sub(offset + 4, offset + 3 + length),
+           offset + 3 + length
 
-  elseif b == 0xDB then -- str 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    local len = bor(
+  elseif byte == 0xDB then -- str 32
+    local i0, i1, i2, i3 = message:byte(offset + 2, offset + 5)
+    local length = bor(
       lshift(i0, 24),
       lshift(i1, 16),
       lshift(i2, 8),
       i3
     )
 
-    return str:sub(ofs + 6, ofs + 5 + len), ofs + 5 + len
+    return message:sub(offset + 6, offset + 5 + length),
+           offset + 5 + length
 
-  elseif b == 0xDC then -- array 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    local len = bor(
+  elseif byte == 0xDC then -- array 16
+    local i0, i1 = message:byte(offset + 2, offset + 3)
+    local length = bor(
       lshift(i0, 8),
       i1
     )
-    local t = table.create(len)
-    local newOfs = ofs + 3
+    local array = table.create(length)
+    local newOffset = offset + 3
 
-    for i=1,len do
-      t[i], newOfs = parse(str, newOfs)
+    for i=1,length do
+      array[i], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return array, newOffset
 
-  elseif b == 0xDD then -- array 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    local len = bor(
+  elseif byte == 0xDD then -- array 32
+    local i0, i1, i2, i3 = message:byte(offset + 2, offset + 5)
+    local length = bor(
       lshift(i0, 24),
       lshift(i1, 16),
       lshift(i2, 8),
       i3
     )
-    local t = table.create(len)
-    local newOfs = ofs + 5
+    local array = table.create(length)
+    local newOffset = offset + 5
 
-    for i=1,len do
-      t[i], newOfs = parse(str, newOfs)
+    for i=1,length do
+      array[i], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return array, newOffset
 
-  elseif b == 0xDE then -- map 16
-    local i0, i1 = str:byte(ofs + 2, ofs + 3)
-    local len = bor(
+  elseif byte == 0xDE then -- map 16
+    local i0, i1 = message:byte(offset + 2, offset + 3)
+    local length = bor(
       lshift(i0, 8),
       i1
     )
-    local t = {}
-    local newOfs = ofs + 3
+    local dictionary = {}
+    local newOffset = offset + 3
     local key
 
     for _=1,len do
-      key, newOfs = parse(str, newOfs)
-      t[key], newOfs = parse(str, newOfs)
+      key, newOffset = parse(message, newOffset)
+      dictionary[key], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return dictionary, newOffset
 
-  elseif b == 0xDF then -- map 32
-    local i0, i1, i2, i3 = str:byte(ofs + 2, ofs + 5)
-    local len = bor(
+  elseif byte == 0xDF then -- map 32
+    local i0, i1, i2, i3 = message:byte(offset + 2, offset + 5)
+    local length = bor(
       lshift(i0, 24),
       lshift(i1, 16),
       lshift(i2, 8),
       i3
     )
-    local t = {}
-    local newOfs = ofs + 5
+    local dictionary = {}
+    local newOffset = offset + 5
     local key
 
     for _=1,len do
-      key, newOfs = parse(str, newOfs)
-      t[key], newOfs = parse(str, newOfs)
+      key, newOffset = parse(message, newOffset)
+      dictionary[key], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return dictionary, newOffset
 
-  elseif b >= 0xE0 then -- negative fixint
-    return 223 - b, ofs + 1
+  elseif byte >= 0xE0 then -- negative fixint
+    return 223 - byte, offset + 1
 
-  elseif b <= 0x7F then -- positive fixint
-    return b, ofs + 1
+  elseif byte <= 0x7F then -- positive fixint
+    return byte, offset + 1
 
-  elseif b - 0x80 <= 0x8F - 0x80 then -- fixmap
-    local len = band(b, 0xF)
-    local t = {}
-    local newOfs = ofs + 1
+  elseif byte - 0x80 <= 0x8F - 0x80 then -- fixmap
+    local length = band(byte, 0xF)
+    local dictionary = {}
+    local newOffset = offset + 1
     local key
 
-    for _=1,len do
-      key, newOfs = parse(str, newOfs)
-      t[key], newOfs = parse(str, newOfs)
+    for _=1,length do
+      key, newOffset = parse(message, newOffset)
+      dictionary[key], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return dictionary, newOffset
 
-  elseif b - 0x90 <= 0x9F - 0x90 then -- fixarray
-    local len = band(b, 0xF)
-    local t = table.create(len)
-    local newOfs = ofs + 1
+  elseif byte - 0x90 <= 0x9F - 0x90 then -- fixarray
+    local length = band(byte, 0xF)
+    local array = table.create(length)
+    local newOffset = offset + 1
 
-    for i=1,len do
-      t[i], newOfs = parse(str, newOfs)
+    for i=1,length do
+      array[i], newOffset = parse(message, newOffset)
     end
 
-    return t, newOfs
+    return array, newOffset
 
-  elseif b - 0xA0 <= 0xBF - 0xA0 then -- fixstr
-    local len = b - 0xA0
-    return str:sub(ofs + 2, ofs + 1 + len), ofs + 1 + len
+  elseif byte - 0xA0 <= 0xBF - 0xA0 then -- fixstr
+    local length = byte - 0xA0
+    return message:sub(offset + 2, offset + 1 + length),
+           offset + 1 + length
 
   end
 end
 
-function MsgPack.decode(str)
-  if str == "" then return end
-  return (parse(str, 0))
+function MsgPack.decode(message)
+  if message == "" then return end
+  return (parse(message, 0))
 end
 
-function MsgPack.encode(t)
+function MsgPack.encode(data)
   error("Stub")
 end
 
